8e1490afc0069cc3e887b1efc4023c1761725b62
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-03 15:57:02 +0530

    add error util

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+    statusCode: number;
+    data: null | object;
+    errors: string[];
+    success: boolean;
+    constructor(
+        statusCode : number,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
