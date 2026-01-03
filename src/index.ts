5c282a3a165ebabde8ea7545c610c6344b879acd
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-03 15:34:05 +0530

    add user model and mothods

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-    
+import express from "express";
+import dotenv from "dotenv";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req : express.Request, res: express.Response) => {
-    res.send('Hello, World! from vishal ');
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello, World! from vishal ");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
